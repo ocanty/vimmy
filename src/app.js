772543ad8b03d91e8d83b8ab1a542bf94845f233
ocanty <git@ocanty.com>
2018-12-15 01:25:29 +0000

    Enviroment variable checking + let/const linting

--- src/app.js
+++ src/app.js
@@ -1,117 +1,136 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
+let express = require('express')
+let path = require('path')
+let favicon = require('serve-favicon')
+let logger = require('morgan')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+let session = require('express-session')
 
-// .env
-var dotenv = require('dotenv');
-dotenv.load();
+// first we load enviromental variables from .env
+let dotenv = require('dotenv')
+dotenv.load()
+
+// next verify every env variable we require was set
+const envRequired = ['DATABASE',
+  'AUTH0_DOMAIN',
+  'AUTH0_CLIENT_ID',
+  'AUTH0_CLIENT_SECRET',
+  'AUTH0_CALLBACK_URL',
+  'PORT',
+  'SESSION_SECRET',
+  'GOOGLE_TRACKING_ID']
+
+envRequired.every((variable) => {
+  if(!process.env[variable]) {
+    console.log('Missing enviromental variable:', variable)
+    process.exit(-1)
+  }
+})
 
 // db
-var mongoose = require("mongoose")
+let mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE)
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function()
-{
-	console.log("Connected to database")
-});
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+
+db.once('open', function() {
+  console.log('Connected to database:', process.env.DATABASE)
+})
 
 // express
-var app = express();
-app.locals.moment = require("moment")
+let app = express()
+app.locals.moment = require('moment')
 
 // auth
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
+var passport = require('passport')
+var Auth0Strategy = require('passport-auth0')
 
 // This will configure Passport to use Auth0
 var strategy = new Auth0Strategy({
-    domain:       process.env.AUTH0_DOMAIN,
-    clientID:     process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
-  }, function(accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  });
+  domain:       process.env.AUTH0_DOMAIN,
+  clientID:     process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL:  process.env.AUTH0_CALLBACK_URL,
+}, function(accessToken, refreshToken, extraParams, profile, done) {
 
-passport.use(strategy);
+
+  // accessToken is the token to call Auth0 API (not needed in the most cases)
+  // extraParams.id_token has the JSON Web Token
+  // profile has all the information from the user
+  return done(null, profile)
+})
+
+passport.use(strategy)
 
 app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: true,
-	saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 // you can use this section to keep a smaller payload
 passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
+  done(null, user)
+})
 
 // express middleware
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use(favicon(__dirname + '/public/favicon.png'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.png'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Routing
-var index = require('./routes/index');
-var user = require('./routes/user');
-var auth = require('./routes/auth');
+var index = require('./routes/index')
+var user = require('./routes/user')
+var auth = require('./routes/auth')
 
-var projects = require("./routes/projects")
-var learn = require("./routes/learn")
-var create = require('./routes/create');
-var discover = require("./routes/discover")
+var projects = require('./routes/projects')
+var learn = require('./routes/learn')
+var create = require('./routes/create')
+var discover = require('./routes/discover')
 
-app.use('/', index);
-app.use('/', auth);
-app.use("/projects", projects)
-app.use('/create', create);
+app.use('/', index)
+app.use('/', auth)
+app.use('/projects', projects)
+app.use('/create', create)
 app.use('/learn',learn)
-app.use('/discover', discover);
-app.use('/users', user);
+app.use('/discover', discover)
+app.use('/users', user)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next)
-{
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
-app.use(function(err, req, res, next)
-{
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error   = req.app.get('env') === 'development' ? err : {};
-	
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error   = req.app.get('env') === 'development' ? err : {}
+  
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
 
-app.listen(process.env.PORT);
+  next()
+})
 
-module.exports = app;
+app.listen(process.env.PORT)
+
+module.exports = app
