40bafcd2135fc091fda5c3a8fad991787cf5863e
ocanty <git@ocanty.com>
2018-12-15 01:44:06 +0000

    linted and documented auth route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,64 +1,75 @@
-var express = require("express");
-var router = express.Router();
-var uuid = require("uuid")
+let express = require('express')
+let router = express.Router()
 
-var passport = require("passport")
+let uuid = require('uuid')
 
-var User = require("../models/db_user.js")
+let passport = require('passport')
 
-// Render the login template
+let User = require('../models/db_user.js')
+
+// GET /login -> send the login template
 router.get('/login', function(req, res)
 {
-	// render homepage with sign in lock
-	res.render("homepage", { title: "Vimmy :: The Educational Virtual Machine", popLock: true });
-});
+  // render homepage and pop up the login Auth0 lock
+  res.render('homepage', {
+    title: 'Vimmy - Login', 
+    popLock: true
+  })
+})
 
-// Perform session logout and redirect to homepage
-router.get('/logout', function(req, res)
-{
-	req.logout();
-	res.redirect('/');
-});
+// GET /logout -> when GET'd logs them out and redirects them home
+router.get('/logout', function(req, res) {
+  req.logout() // passport .logout
+  res.redirect('/')
+})
 
-// Perform the final stage of authentication and redirect to '/user'
-router.get('/callback', passport.authenticate('auth0', { failureRedirect: '/failed' }),
-	function(req, res)
-	{
-		// check if this user exists
-		User.findOne({ auth_id: req.user.id },
-			function(error,user)
-			{
-				if(error){ return console.error(error); }
-				
-				// if they do populate the session with read-only db values
-				if(user)
-				{
-					req.user.db = user
-					res.redirect(req.session.returnTo || '/');
-				}
-				else // if they dont add them to the database
-				{
-					var new_user = new User(
-					{ 
-						auth_id: req.user.id,
-						user_id: uuid.v4().substring(0,6),
-						display_name: req.user.nickname,
-						account_created: new Date(),
-						avatar: "/img/user-avatar.png",
-						email: req.user.emails[0].value,
-						score: 0
-					})
-					
-					new_user.save(function(error,new_user)
-					{
-						if(error){ return console.error(error); }
-						req.user.db = new_user
-						res.redirect(req.session.returnTo || '/');
-					})
-				}
-			}
-		)
-	}
-);
+/*
+  GET /callback 
+  When a user finishes authentication with Auth0, it sends them back here
+  Process Auth0's stuff from our side and redirect them to the homepage
+*/
+router.get('/callback', 
+  passport.authenticate('auth0', { 
+    failureRedirect: '/failed' 
+  }),
+
+  function(req, res) {
+
+    // check if the user exists
+    User.findOne({ auth_id: req.user.id }, 
+      function(err, user) {
+        if(err){ return console.error(err) }
+          
+        // if they do exist populate their session with (read-only) db values
+        // these will not be updated if modified!
+        if(user) {
+          req.user.db = user
+
+          // return them to where they came from or the homepage
+          res.redirect(req.session.returnTo || '/')
+        }
+        else // if they dont add them to the database (they're a new user)
+        {
+          let new_user = new User({ 
+            auth_id: req.user.id,
+            user_id: uuid.v4().substring(0,6),
+            display_name: req.user.nickname,
+            account_created: new Date(),
+            avatar: '/img/user-avatar.png',
+            email: req.user.emails[0].value,
+            score: 0
+          })
+          
+          new_user.save(function(err,new_user) {
+            if(err){ return console.error(err) }
+            req.user.db = new_user
+
+            res.redirect(req.session.returnTo || '/') 
+          })
+        }
+      }
+    )
+  }
+)
 
 module.exports = router
