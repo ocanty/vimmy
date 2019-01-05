1d2a126fb174b0ad3a32222c1f8c732a410cacf7
ocanty <git@ocanty.com>
2019-01-05 03:54:00 +0000

    Linted discover.js

--- src/routes/discover.js
+++ src/routes/discover.js
@@ -1,135 +1,61 @@
-var express = require('express');
-var router = express.Router();
-var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+let express = require('express');
+let router = express.Router();
 
-var Project = require("../models/db_project")
+let Project = require('../models/db_project')
 
-router.get("/",function(req,res,next)
-{
-	var isLoggedIn = (req.isAuthenticated && req.isAuthenticated())
-	res.render("discover",
-	{
-		title: "Vimmy :: Discover",
-		loggedIn: isLoggedIn, 
-		user: (!isLoggedIn ? { } : req.user.db),
-	})
+// This router is used in app.js as /discover
+
+// GET / -> serve the discover search dashboard
+router.get('/',function(req,res) {
+  let isLoggedIn = (req.isAuthenticated && req.isAuthenticated())
+
+  res.render('discover', {
+    title: 'Vimmy - Discover',
+    loggedIn: isLoggedIn, 
+    user: (!isLoggedIn ? { } : req.user.db),
+  })
 })
 
-// send projects based off serach paramaters
-router.get('/projects', function(req, res, next)
-{
-	var category = req.query.category || "All"
-	var sort = req.query.sort || "score"
-	var amount = req.query.amount || 10 
-	var from = req.query.from || 0
-	
-	var find_params = { isPublished: true }
-	var sort_param = { }
-	if(category != "All"){ find_params.category = category }
-	if(sort == "recent") { sort_param = { created_at: -1 }}
-	if(sort == "score") { sort_param = { num_votes: -1 }}
-	if(sort == "views") { sort_param = { views: -1 }}
-	
-	var outdata = { }
-	
-	Project.find(find_params).sort(sort_param).limit(100).exec(function(err, projects) 
-	{
-		if(err || !projects)
-		{
-			console.log(err)
-			res.status(404).send("{}")
-			return
-		}
-		else
-		{
-			outdata.projects = projects.slice(from,from+amount)
-			
-			res.send(JSON.stringify(outdata))
-		}
-	});
-	
-	// Project.find().populate({path :"comments.poster"}).exec(function(error,project)
-	// {
-		// if(!checkProjectVisAuth(req,res,project)) return
-		
-		// if(error || !project)
-		// {
-			// res.status(404).send("{}")
-			// return
-		// }
-		// else
-		// {
-			// var comments = project.comments
-			// var sort = req.query.sort
-			// var amount = parseInt(req.query.amount) || 10
-			// var from = parseInt(req.query.from) || 0
-			// console.log(comments,sort,amount)
-			// var outdata = { }
-			
-			// switch(sort)
-			// {
-				// case "recent":
-					// comments.sort(function(a,b){
-						// a = new Date(b.created_at);
-						// b = new Date(a.created_at);
-						// return a>b ? -1 : a<b ? 1 : 0;
-					// });
-					
-					// outdata.comments = comments.slice(from,from+amount)
-					
-					//strip sensitive info
-					// for(var n in outdata.comments)
-					// {
-						// var user = outdata.comments[n].poster
-						// outdata.comments[n].poster = { }
-						// outdata.comments[n].poster = 
-						// { 
-							// display_name: user.display_name,
-							// user_id: user.user_id,
-							// avatar: user.avatar
-						// }
-					// }
-					
-					
-					// res.send(JSON.stringify(outdata))
-					// return
-				// break;
-				
-				// case "score":
-					// comments.sort(function(a,b){
-						//Turn your strings into dates, and then subtract them
-						//to get a value that is either negative, positive, or zero.
-						// return b.score - a.score
-					// });
-					
-					// outdata.comments = comments.slice(from,from+amount)
-					
-					//strip sensitive info
-					// for(var n in outdata.comments)
-					// {
-						// var user = outdata.comments[n].poster
-						// outdata.comments[n].poster = { }
-						// outdata.comments[n].poster = 
-						// { 
-							// display_name: user.display_name,
-							// user_id: user.user_id,
-							// avatar: user.avatar
-						// }
-					// }
-					
-					
-					// res.send(JSON.stringify(outdata))
-					// return
-				// break
-				
-				// default:
-					// res.status(404).send("404 Not Found")
-				// break
-			// }
-		// }
-	// })
+// GET /projects -> search parameters passed in query
+router.get('/projects', function(req, res) {
+  
+  // if they supplied a category use that or search every category
+  let category = req.query.category || 'All'
 
-	return
-});
+  // sort by their requested sort or by score
+  let sort = req.query.sort || 'score'
+
+  // give them how many they want or 10
+  let amount = req.query.amount || 10 
+
+  // start from their page or 0
+  let from = req.query.from || 0
+  
+  // only search published projects
+  let find_params = { isPublished: true }
+
+  // set up mongoose search 
+  let sort_param = { }
+  if(category != 'All') { find_params.category = category }
+  if(sort == 'recent')  { sort_param = { created_at: -1 }}
+  if(sort == 'score')   { sort_param = { num_votes: -1 }}
+  if(sort == 'views')   { sort_param = { views: -1 }}
+  
+  // now find their project
+  Project.find(find_params)
+    .sort(sort_param)
+    .limit(100)
+    .exec(function(err, projects) {
+      if(err || !projects) {
+        console.log(err)
+        res.status(404).send('{}')
+        return
+      }
+
+      let out = { }
+      out.projects = projects.slice(from,from+amount)
+      res.send(JSON.stringify(out))
+    })
+})
 
 module.exports = router
