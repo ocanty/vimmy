2331b55831044393962b65f91b0a4acea2e45319
ocanty <git@ocanty.com>
2019-01-05 03:54:54 +0000

    Linted utilities.js

--- src/routes/utilities.js
+++ src/routes/utilities.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express')
+let router = express.Router()
 
-var multer = require("multer")
-var fs = require("fs")
-var mime = require("mime")
+let multer = require('multer')
+let fs = require('fs')
+let mime = require('mime')
 
-var upload = multer({ dest: "uploads/"})
+let upload = multer({ dest: 'uploads/'})
 
 // future use case
 
-module.exports = router;
+module.exports = router
