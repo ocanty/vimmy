38e6c06e058d1b4ce294d621323d9e4a4156f7a4
ocanty <git@ocanty.com>
2019-01-05 03:54:46 +0000

    Linted user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,24 +1,26 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
-var User = require("../models/db_user")
+let User = require('../models/db_user')
 
-/* GET users listing. */
-router.get('/:id', function(req, res, next) {
-	
-	User.findOne({user_id: req.params.id}).populate("projects").exec(function(err,user)
-		{
-			if(err || !user) res.status(404).send("Not Found")
-		
-			var isLoggedIn = (req.isAuthenticated && req.isAuthenticated())
-			res.render("user",{
-				
-				title: "Vimmy :: " + user.display_name,
-				loggedIn: isLoggedIn, 
-				user: (!isLoggedIn ? { } : req.user.db),
-				render_user:user})
-		
-	})
-});
+// This router is used in app.js as /user/
 
-module.exports = router;
+// GET /<id> -> Show a users profile by id
+router.get('/:id', function(req, res) {
+  User.findOne({user_id: req.params.id})
+    .populate('projects')
+    .exec(function(err,user) {
+      if(err || !user) res.status(404).send('Not Found')
+    
+      let isLoggedIn = (req.isAuthenticated && req.isAuthenticated())
+
+      res.render('user',{
+        title: 'Vimmy - ' + user.display_name,
+        loggedIn: isLoggedIn, 
+        user: (!isLoggedIn ? { } : req.user.db),
+        render_user: user
+      })
+    })
+})
+
+module.exports = router
