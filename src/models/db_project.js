40f8d87f52cec9fa7ae39e5db2c6d72c8c234372
ocanty <git@ocanty.com>
2019-01-06 05:36:27 +0000

    Linted DB models

--- src/models/db_project.js
+++ src/models/db_project.js
@@ -1,41 +1,45 @@
 
-var mongoose = require("mongoose")
+let mongoose = require('mongoose')
 
-// Comment Scheme
+/*
+  Comment Schema
+*/
 var comment_schema = new mongoose.Schema({
-	comment_id: {type:String, required: true},
-	score: { type: Number, default: 0 },
-	comment:  { type: String, required: true },
-	posted_at: { type: Date, required: true } ,
-	poster:  { type: mongoose.Schema.Types.ObjectId, required: true, ref: "DB_User" }
-});
-
-// Project Scheme
-var project_schema = new mongoose.Schema(
-{
-	project_id: {type:String, unique: true, index: { unique: true } },
-	name: { type:String, required: true },
-	category: { type:String, required: true, default: 0},
-	isPublished: { type:Boolean, required: true, default: true},
-	
-	creator: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "DB_User"},
-	created_at: { type: Date, required: true },
-	updated_at: { type: Date, required: true },
-	
-	vm_version: { type:Number, required: true },
-	code: { type:String, required: true },
-	data: { type:String, required: true },
-	
-	// we do this to make sorting easier later rather than having to access votes.length, i.e mongoose does not support check params of an array for its sorting methods
-	num_votes: { type:Number, default: 0 },
-	votes: 
-	[
-		{ type: mongoose.Schema.Types.ObjectId, required: true, ref: "DB_User"}
-	],
-	
-	views: { type: Number, default: 0 },
-	
-	comments: [comment_schema]
+  comment_id:   { type:String, required: true },
+  score:        { type: Number, default: 0 },
+  comment:      { type: String, required: true },
+  posted_at:    { type: Date, required: true },
+  poster:       { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'DB_User' }
 })
 
-module.exports = mongoose.model("DB_Project",project_schema)
+/*
+  Project Schema
+*/
+var project_schema = new mongoose.Schema({
+  project_id:   { type:String, unique: true, index: { unique: true } },
+  name:         { type:String, required: true },
+  category:     { type:String, required: true, default: 0 },
+  isPublished:  { type:Boolean, required: true, default: true },
+  
+  creator:      { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'DB_User' },
+  created_at:   { type: Date, required: true },
+  updated_at:   { type: Date, required: true },
+  
+  vm_version:   { type:Number, required: true },
+  code:         { type:String, required: true },
+  data:         { type:String, required: true },
+  
+  // we do this to make sorting easier later rather than having to access votes.length, i.e mongoose does not support check params of an array for its sorting methods
+  num_votes: { type:Number, default: 0 },
+  
+  votes: 
+  [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'DB_User'}
+  ],
+  
+  views: { type: Number, default: 0 },
+  
+  comments: [comment_schema]
+})
+
+module.exports = mongoose.model('DB_Project', project_schema)
--- src/models/db_user.js
+++ src/models/db_user.js
@@ -1,34 +1,29 @@
 
-var mongoose = require("mongoose")
+let mongoose = require('mongoose')
 
-var user_schema = new mongoose.Schema(
-{
-	// passed by auth0 passport
-	auth_id: { type: String, required: true },
-	
-	user_id: { type: String, required: true, index: { unique: true } },
-	email: { type: String, required: true },
-	
-	// future plan for changeable display names
-	display_name: { type: String, required: true },
-	account_created: { type: Date, required: true },
-	avatar: { type: String, default: "/img/user-avatar.png"},
-	score: { type: Number, required: true},
-	
-	// array of projects
-	projects: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "DB_Project"
-		}
-	],
-	
-	// last lessons they visited eg. "1/2"
-	recent_lessons: [
-		{
-			type: String
-		}
-	]
+let user_schema = new mongoose.Schema({
+  // passed by auth0 passport
+  auth_id: { type: String, required: true },
+  
+  user_id: { type: String, required: true, index: { unique: true } },
+  email: { type: String, required: true },
+  
+  // future plan for changeable display names
+  display_name: { type: String, required: true },
+  account_created: { type: Date, required: true },
+  avatar: { type: String, default: '/img/user-avatar.png'},
+  score: { type: Number, required: true},
+  
+  // array of projects
+  projects: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'DB_Project'
+  }],
+  
+  // last lessons they visited eg. "1/2"
+  recent_lessons: [{
+    type: String
+  }]
 })
 
-module.exports = mongoose.model("DB_User",user_schema)
+module.exports = mongoose.model('DB_User', user_schema)
